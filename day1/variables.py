b64842eeaeae530dfd0ee39fc303942b1a33a3a1
kirtimchoubey1226-eng <kirtimchoubey1226@gmail.com>
2025-12-28 17:47:20 +0530

    Day 1: Variables and data types

--- day1/variables.py
+++ day1/variables.py
@@ -1,2 +1,23 @@
 # Day 1: Python Basics
 print("Hello Python")
+# Day 1: Python Basics
+
+name = "Kirti"
+age = 20
+height = 5.6
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+a = 10
+b = 5
+print("Addition:", a + b)
+print("Multiplication:", a * b)
